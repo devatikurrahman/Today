abc6a6fc3c455b7fffddd88b0b9e7a08f8cd8284
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-10 03:53:04 +0600

    Create a custom button action

---
+++ Today/ListViewController/ReminderDoneButton.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Atikur Rahman on 9/10/23.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}
+
+
