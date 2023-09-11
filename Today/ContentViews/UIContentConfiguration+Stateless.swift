63ae0f95f00de4f0cb66f43d5b6b2688f1c6f105
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-11 22:31:51 +0600

    Conform to the content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Atikur Rahman on 9/11/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
