baa42cb2651c5fcb99b0c599b9e5a9a49fb81954
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-13 22:11:21 +0600

    Add action to segmented control

--- Today/ListViewController/ReminderListViewController+Actions.swift
+++ Today/ListViewController/ReminderListViewController+Actions.swift
@@ -31,4 +31,9 @@ extension ReminderListViewController {
     @objc func didCancleAdd(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+    
+    @objc func didChangeListStyle(_ sender: UISegmentedControl) {
+        listStyle = ReminderListStyle(rawValue: sender.selectedSegmentIndex) ?? .today
+        updateSnapshot()
+    }
 }
